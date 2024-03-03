390193254c371b06359f598c7298fb020451ce4e
amir3170 <amir3170@gmail.com>
2024-03-03 20:36:51 +0200

    delete working

--- db_operations.py
+++ db_operations.py
@@ -1,8 +1,17 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+
 # db_operations.py
 def get_all_tasks(db):
     return db.tasks.find()
 
-def add_task(db, task):
-    db.tasks.insert_one(task)
-
-# Implement more database operations as needed
+def add_task(db, task_name, task_status, task_info):
+    # Insert the new task into the tasks collection
+    db.tasks.insert_one({'name': task_name, 'status': task_status, 'info': task_info})
+    
+def delete_task(db, task_id):
+    # Converts the string task_id to an ObjectId because MongoDB uniquely identifies documents using ObjectId
+    #not strings, ensuring accurate document operations.
+    task_id_obj = ObjectId(task_id)
+    db.tasks.delete_one({'_id': task_id_obj})
